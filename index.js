ab832ae8ffdf149e5dc5ced20002c2850b2ebdb9
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-04-29 00:42:30 +0530

    cicd

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-const str = 'hello_chandan'
+// const str = 'hello_chandan'
 
-console.log(str)
+// console.log(str)
 
-const str1 = 'world'
+// const str1 = 'world'
 
-console.log(str + ' ' + str1)
+// console.log(str + ' ' + str1)
 
-if (str.length > str1.length) {
-  console.log('true')
-} else {
-  console.log('false')
-}
+// if (str.length > str1.length) {
+//   console.log('true')
+// } else {
+//   console.log('false')
+// }
+
+function additation(a, b) {
+    return a + b;
+  }
+  
+  function substaraction(a, b) {
+    return a - b;
+  }
+  
+  function division(a, b) {
+    if (b === 0) {
+      throw new Error("Not divider by zero");
+    }
+    return a / b;
+  }
+   function multiplication(a, b) {
+    return a * b;
+  }
+  console.log(additation(3,4));
+  console.log(substaraction(9,3));
+  console.log(multiplication(3,4));
+  console.log(division(6,2));
+
+
+
+  

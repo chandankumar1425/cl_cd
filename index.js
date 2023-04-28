1337fc56ff98fa543fc9553a9c8f9c2fc09df2d9
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-04-28 15:15:34 +0530

    chandan

---
+++ index.js
@@ -0,0 +1,13 @@
+const str = 'hello_chandan'
+
+console.log(str)
+
+const str1 = 'world'
+
+console.log(str + ' ' + str1)
+
+if (str.length > str1.length) {
+  console.log('true')
+} else {
+  console.log('false')
+}
